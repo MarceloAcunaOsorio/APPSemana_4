bb67e41b2fac4d26cdb10c68b62e934be9fba6f7
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-08-31 20:41:15 -0400

    se crea login

--- app/src/main/java/com/marceloacuna/appsemana4/LoginScreen.kt
+++ app/src/main/java/com/marceloacuna/appsemana4/LoginScreen.kt
@@ -1,11 +1,18 @@
 package com.marceloacuna.appsemana4
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,6 +38,39 @@ fun LoginScreen() {
 
       //texto de pantalla de inicio y su estilo
       Text(text = "Bienvenido My App", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+      //salto de linea
+      Spacer(modifier = Modifier.height(4.dp))
+      
+      Text(text = "Ingrese con su cuenta")
+
+      //salto de linea
+      Spacer(modifier = Modifier.height(16.dp))
+
+      //Input para ingresar el email
+      OutlinedTextField(value = "", onValueChange = {}, label = {Text(text = "Email")})
+
+      //salto de linea
+      Spacer(modifier = Modifier.height(16.dp))
+
+      //Input para ingresar el Contraseña
+      OutlinedTextField(value = "", onValueChange = {}, label = {Text(text = "Contraseña")})
+      
+      //salto de linea
+      Spacer(modifier = Modifier.height(16.dp))
+      
+      Button(onClick = {}) {
+         Text(text = "Login")
+      }
+
+      //salto de linea
+      Spacer(modifier = Modifier.height(32.dp))
+
+
+         Text(text = "Recuperar Contraseña?", modifier = Modifier.clickable {
+
+         })
+
    }
 }
 
